68bd2aa093ace349522c296fa6820b83245b0dea
Esa Kurniawan <esakurniawan866@gmail.com>
2022-10-03 19:58:43 +0800

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,12 @@ import Head from "next/head";
 import Dictionary from "components/screens/dictionary";
 
 const DictionaryPage: NextPage = () => {
+    console.log(process.env.NEXT_PUBLIC_DICTIONARY_API_BASE_URL);
+
     return (
         <>
             <Head>
-                <title>Open Dictionary</title>
+                <title>Quick Dictionary</title>
             </Head>
 
             <Dictionary />
