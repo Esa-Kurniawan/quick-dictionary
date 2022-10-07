3323b21dada87411158c97b6bf40b68f2f123f66
Esa Kurniawan <esakurniawan866@gmail.com>
2022-10-07 12:52:56 +0800

    added headesrs in the reques

--- components/screens/dictionary/proxies/getLexicon.ts
+++ components/screens/dictionary/proxies/getLexicon.ts
@@ -1,14 +1,22 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { Endpoint, Language } from "types";
 
 import { Lexicon } from "../types";
+
+const config: AxiosRequestConfig = {
+    headers: {
+        app_id: process.env.NEXT_PUBLIC_OXFORD_DICTIONARIES_APP_ID!,
+        app_key: process.env.NEXT_PUBLIC_OXFORD_DICTIONARIES_APP_ID!,
+    },
+};
 
 export const getLexicon = async (
     language: Language,
     word: string
 ): Promise<Lexicon> => {
     const { data: lexicon } = await axios.get(
-        `${process.env.NEXT_PUBLIC_OXFORD_DICTIONARIES_BASE_URL}/${Endpoint.ENTRIES}/${language}/${word}`
+        `${process.env.NEXT_PUBLIC_OXFORD_DICTIONARIES_BASE_URL}/${Endpoint.ENTRIES}/${language}/${word}`,
+        config
     );
     return lexicon;
 };
