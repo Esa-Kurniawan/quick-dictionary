8596cbc13b0cd8475dbd2b8a9293492b97589252
Esa Kurniawan <esakurniawan866@gmail.com>
2022-10-08 11:12:40 +0800

    added

--- components/screens/dictionary/components/LexiconResult/ListOfDefinitions.tsx
+++ components/screens/dictionary/components/LexiconResult/ListOfDefinitions.tsx
@@ -6,15 +6,14 @@ interface ListOfDefinitionsProps {
 
 const ListOfDefinitions = ({ definitions }: ListOfDefinitionsProps) => (
     <ol className="pl-12 flex flex-col gap-2 mt-2 list-decimal">
-        {definitions.map((definition, definitionIndex) => {
-            if (definition.hasOwnProperty("example"))
-                return (
-                    <li key={definitionIndex}>
-                        <h3>{definition.definition}</h3>
-                        <p className="">{`"${definition.example}"`}</p>
-                    </li>
-                );
-        })}
+        {definitions.map((definition, definitionIndex) => (
+            <li key={definitionIndex}>
+                <h3>{definition.definition}</h3>
+                {definition.hasOwnProperty("example") && (
+                    <p className="">{`"${definition.example}"`}</p>
+                )}
+            </li>
+        ))}
     </ol>
 );
 
